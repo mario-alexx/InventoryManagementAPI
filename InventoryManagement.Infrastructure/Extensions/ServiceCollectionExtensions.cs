7b8c207917b2d1c5bf30a374c6542d5c05ec09bf
mario-alexx <marioatoroh@gmail.com>
2024-07-01 18:06:59 -0300

    adding extensions methods

---
+++ InventoryManagement.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using InventoryManagement.Core.Repositories;
+using InventoryManagement.Infrastructure.Data;
+using InventoryManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace InventoryManagement.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Extension methods for setting up infrastructure services in an <see cref="IServiceCollection" />.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the infrastructure services to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<InventoryDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
