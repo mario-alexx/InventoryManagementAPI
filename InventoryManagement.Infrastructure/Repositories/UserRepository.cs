42bf44c7454f8e47243a7b66c7fca7a68a9dd280
mario-alexx <marioatoroh@gmail.com>
2024-07-01 18:09:26 -0300

    adding user repository implementation

---
+++ InventoryManagement.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using InventoryManagement.Core.Entities;
+using InventoryManagement.Core.Repositories;
+using InventoryManagement.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Repositories
+{
+    /// <summary>
+    /// User repository implementation.
+    /// </summary>
+    public class UserRepository : Repository<User>, IUserRepository
+    {        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRepository"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public UserRepository(DbContext context) : base(context)
+        {  
+        }
+
+        // Add specific methods for User
+    }
+}
