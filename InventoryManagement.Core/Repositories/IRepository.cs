aa44b89c9df46bb5b1cb2cd4ce0f7727dbc8acd3
mario-alexx <marioatoroh@gmail.com>
2024-07-01 12:44:01 -0300

    adding generic repository

---
+++ InventoryManagement.Core/Repositories/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Core.Repository
+{
+    /// <summary>
+    /// Generic repository interface for performing CRUD operations.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Gets an entity by its identifier.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The entity.</returns>
+        Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Gets all entities.
+        /// </summary>
+        /// <returns>A list of all entities.</returns>
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// Finds entities based on a predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to filter entities.</param>
+        /// <returns>A list of entities that match the predicate.</returns>
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        /// <summary>
+        /// Adds a new entity.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        Task AddAsync(T entity);
+
+        /// <summary>
+        /// Updates an existing entity.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        void Update(T entity);
+
+        /// <summary>
+        /// Removes an entity.
+        /// </summary>
+        /// <param name="entity">The entity to remove.</param>
+        void Remove(T entity);
+    }
+}
