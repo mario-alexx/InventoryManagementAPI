abd7a1c07c32403890ade5f3c029edb795f1284f
mario-alexx <marioatoroh@gmail.com>
2024-07-01 18:09:11 -0300

    adding product repository implementation

---
+++ InventoryManagement.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagement.Core.Entities;
+using InventoryManagement.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Repositories
+{
+    /// <summary>
+    /// Product repository implementation.
+    /// </summary>
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductRepository"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+
+        // Add specific methods for Product
+    }
+}
