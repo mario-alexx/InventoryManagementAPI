e4826ed295c95f067f37e7697b0e54eb576c13c2
mario-alexx <marioatoroh@gmail.com>
2024-07-01 20:27:05 -0300

    adding product dto

---
+++ InventoryManagement.Application/DTOs/ProductDto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Application.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for Product.
+    /// </summary>
+    public class ProductDTO
+    {
+        /// <summary>
+        /// Gets or sets the product identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the product.
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the description of the product.
+        /// </summary>
+        public string Description { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the price of the product.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity of the product in stock.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the product image.
+        /// </summary>
+        public string UrlImage { get; set; } = null!;
+    }
+}
