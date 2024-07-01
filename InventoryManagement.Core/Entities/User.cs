8e05e82ea36b15631a8938a96b6c191b7b111986
mario-alexx <marioatoroh@gmail.com>
2024-07-01 15:53:43 -0300

    change user entity

--- InventoryManagement.Core/Entities/User.cs
+++ InventoryManagement.Core/Entities/User.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace InventoryManagement.Core.Entities
 {
-    internal class User
+    /// <summary>
+    /// Represents a user in the system.
+    /// </summary>
+    public class User
     {
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        public string Username { get; set; } = null!; 
+
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        public string Password { get; set; } = null!; 
+         
+        /// <summary>
+        /// Gets or sets the email of the user.
+        /// </summary>
+        public string Email { get; set; } = null!;
     }
 }
