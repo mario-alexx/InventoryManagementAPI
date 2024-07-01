a0b434c63bf81a90dcf3917782afc3fa2456d862
mario-alexx <marioatoroh@gmail.com>
2024-07-01 20:27:20 -0300

    adding user dto

---
+++ InventoryManagement.Application/DTOs/UserDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Application.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for Product.
+    /// </summary>
+    public class UserDTO
+    {
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user.
+        /// </summary>
+        public string Username { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the password of the user.
+        /// </summary>
+        public string Password { get; set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the email of the user.
+        /// </summary>
+        public string Email { get; set; } = null!;
+    }
+}
