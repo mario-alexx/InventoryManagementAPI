cce7664982fc89e46cd7037420b2d7311ba73367
mario-alexx <marioatoroh@gmail.com>
2024-07-03 18:41:12 -0300

    adding auth methods

--- InventoryManagement.Infrastructure/Repositories/UserRepository.cs
+++ InventoryManagement.Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using InventoryManagement.Core.Entities;
 using InventoryManagement.Core.Repositories;
 using InventoryManagement.Core.Repository;
+using InventoryManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -14,15 +15,35 @@ namespace InventoryManagement.Infrastructure.Repositories
     /// User repository implementation.
     /// </summary>
     public class UserRepository : Repository<User>, IUserRepository
-    {        
+    {
+        private readonly InventoryDbContext _context;
         /// <summary>
         /// Initializes a new instance of the <see cref="UserRepository"/> class.
         /// </summary>
-        /// <param name="context">The database context.</param>
-        public UserRepository(DbContext context) : base(context)
+        /// <param name="context">The application database context.</param>
+        public UserRepository(InventoryDbContext context) : base(context)
         {  
+            _context = context;
         }
 
-        // Add specific methods for User
+        /// <summary>
+        /// Gets a user by their email.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <returns>The user entity.</returns>
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Email == email);
+        }
+
+        /// <summary>
+        /// Adds a new user.
+        /// </summary>
+        /// <param name="user">The user entity.</param>
+        public async Task AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
     }
 }
