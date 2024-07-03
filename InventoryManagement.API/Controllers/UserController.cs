ed162edb7289deee9074fa211a23be08b5b81484
mario-alexx <marioatoroh@gmail.com>
2024-07-03 20:49:53 -0300

    adding user controller

---
+++ InventoryManagement.API/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using InventoryManagement.Application.DTOs;
+using InventoryManagement.Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Controllers
+{
+    /// <summary>
+    /// Controller for managing users.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UsersController"/> class.
+        /// </summary>
+        /// <param name="userService">The user service.</param>
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <returns>A list of users.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        /// <summary>
+        /// Gets a user by its identifier.
+        /// </summary>
+        /// <param name="id">The user identifier.</param>
+        /// <returns>The user.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Adds a new user.
+        /// </summary>
+        /// <param name="userDto">The user DTO.</param>
+        /// <returns>The added user.</returns>
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> AddUser([FromBody] UserDTO userDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userService.AddUserAsync(userDto);
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
+
+        /// <summary>
+        /// Updates an existing user.
+        /// </summary>
+        /// <param name="id">The user identifier.</param>
+        /// <param name="userDto">The user DTO.</param>
+        /// <returns>The updated user.</returns>
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserDTO>> UpdateUser([FromBody] UserDTO userDto, int id)
+        {
+            if(id != userDto.Id)
+            {
+                return BadRequest("User ID mismatch");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var updatedUser = await _userService.UpdateUserAsync(userDto);
+            return Ok(updatedUser);
+        }
+
+        /// <summary>
+        /// Deletes a user.
+        /// </summary>
+        /// <param name="id">The user identifier.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if(user == null) 
+            { 
+                return NotFound();
+            }
+
+            await _userService.DeleteUserAsync(id);
+            return NoContent();
+        }
+    }
+}
