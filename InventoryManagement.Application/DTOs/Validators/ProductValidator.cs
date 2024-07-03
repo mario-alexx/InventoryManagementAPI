1f334de82da1b9ebfe52b33eadd0d04373669dad
mario-alexx <marioatoroh@gmail.com>
2024-07-03 11:25:03 -0300

    adding validators class with fluent validation

---
+++ InventoryManagement.Application/DTOs/Validators/ProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Application.DTOs.Validators
+{
+    /// <summary>
+    /// Validator for ProductDto.
+    /// </summary>
+    public class ProductValidator : AbstractValidator<ProductDTO>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductDtoValidator"/> class.
+        /// </summary>
+        public ProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .MaximumLength(100).WithMessage("Product name must not exceed 100 characters.");
+
+            RuleFor(d => d.Description)
+                .NotEmpty().WithMessage("Product description is required.");
+
+            RuleFor(p => p.Price)
+                .GreaterThan(0).WithMessage("Product price must be greater than zero.");
+
+            RuleFor(q => q.Quantity)
+                .GreaterThan(0).WithMessage("Product quantity must be zero or greater.");
+
+            RuleFor(url => url.UrlImage)
+                .NotEmpty().WithMessage("Product image is rquired.")
+                .Must(uri => Uri.IsWellFormedUriString(uri, UriKind.Absolute)).WithMessage("Invalid URL format.");
+                
+        
+        }  
+    }
+}
---
+++ InventoryManagement.Application/DTOs/Validators/UserDtoValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Application.DTOs.Validators
+{
+    /// <summary>
+    /// Validator for UserDto.
+    /// </summary>
+    public class UserDtoValidator : AbstractValidator<UserDTO>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserDtoValidator"/> class.
+        /// </summary>
+        public UserDtoValidator()
+        {
+            RuleFor(u => u.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MaximumLength(50).WithMessage("Username must not exceed 50 characters.");
+
+            RuleFor(p => p.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+
+            RuleFor(e => e.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.s");
+        }
+    }
+}
