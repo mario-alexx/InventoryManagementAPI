d9ef0b401050157dbbb3905f9096b22d0982c12b
mario-alexx <marioatoroh@gmail.com>
2024-07-03 20:45:23 -0300

    adding user repository methods

--- InventoryManagement.Core/IRepositories/IUserRepository.cs
+++ InventoryManagement.Core/IRepositories/IUserRepository.cs
@@ -9,10 +9,21 @@ using System.Threading.Tasks;
 namespace InventoryManagement.Core.Repositories
 {
     /// <summary>
-    /// Repository interface for User entity.
+    /// Repository interface for User repository.
     /// </summary>
     public interface IUserRepository : IRepository<User>
     {
-        // Add specific methods for user
+        /// <summary>
+        /// Gets a user by their email.
+        /// </summary>
+        /// <param name="email">The email of the user.</param>
+        /// <returns>The user entity.</returns>
+        Task<User> GetUserByEmailAsync(string email);
+
+        /// <summary>
+        /// Adds a new user.
+        /// </summary>
+        /// <param name="user">The user entity.</param>
+        Task AddUserAsync(User user);
     }
 }
